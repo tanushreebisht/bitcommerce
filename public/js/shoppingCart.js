6f6518904e4a0540d0e56ed354d7df675ae67324
Chris Ta <CTaCDE@users.noreply.github.com>
2018-02-25 21:01:49 -0800

    Appending current cart amount

--- public/js/shoppingCart.js
+++ public/js/shoppingCart.js
@@ -1,17 +1,28 @@
 
 function printCartQuant(cart_total) {
-        paypal.minicart.view.toggle();
+    paypal.minicart.view.toggle();
 
-    $('.form-text').change(function() {
-        container_box = document.getElementById(cart_total);
-        html_string = paypal.minicart.cart.total();
-        container_box.insertAdjacentHTML('beforeend',html_string);
-    });
+    var html_string = "";
+    html_string = "$" + paypal.minicart.cart.total() + ".00";
+    console.log("Current cart amount: " + html_string);
+
+    var container_box; 
+    container_box = document.getElementById(cart_total);
+    container_box.insertAdjacentHTML('afterbegin',html_string);
+
+/*
+var parent = document.createElement("div");
+var child = document.createElement("p");
+parent.appendChild(child);
+var span = document.createElement("span");
+
+child.replaceWith(span);
+*/
+
 }
 
 function addToCart() {
-        paypal.minicart.view.toggle();
-
+    paypal.minicart.view.toggle();
     paypal.minicart.cart.add({ 
         "business": "user@example.com",
         "item_name": "Item Name",
@@ -22,8 +33,7 @@ function addToCart() {
 }
 
 function clearCart(){
-        paypal.minicart.view.toggle();
-
+    paypal.minicart.view.toggle();
     paypal.minicart.cart.destroy();
     printCartQuant("cart_total");
 }
